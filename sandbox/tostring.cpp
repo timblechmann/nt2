9f5fd98702e1fc0028eb704821226a697355626a
Mathias Gaunard <mathias@gaunard.com>
2011-04-24 19:59:01 +0200

    use state for terminals in tostring sandbox

--- sandbox/tostring.cpp
+++ sandbox/tostring.cpp
@@ -1,12 +1,15 @@
 #include <nt2/sdk/simd/pack.hpp>
+#include <nt2/sdk/simd/pack/io.hpp>
 #include <nt2/sdk/details/type_id.hpp>
+
+#include <boost/lexical_cast.hpp>
 
 namespace nt2
 {
 
 namespace tag
 {
-  NT2_HIERARCHY_CLASS(tostring_, formal_);
+  NT2_HIERARCHY_CLASS(tostring_, meta::unspecified_<tostring_>);
 }
   
 namespace meta
@@ -15,14 +18,14 @@ namespace meta
   struct tostring_
       : boost::proto::
         unpack< boost::proto::
-  call< functor<Tag, tag::tostring_> >( boost::proto::_ )
+                call< functor<Tag, tag::tostring_> > ( boost::proto::_, boost::proto::_state )
               >
   {};
   
   template <>
   struct tostring_<tag::terminal_>
       : boost::proto::
-  call< functor<tag::terminal_, tag::tostring_> ( boost::proto::_value ) >
+        call< functor<tag::terminal_, tag::tostring_> ( boost::proto::_value, boost::proto::_state ) >
   {
   };
   
@@ -35,8 +38,33 @@ namespace boost { namespace proto
         : boost::mpl::true_  {};
 } }
 
-NT2_REGISTER_DISPATCH ( Tag , tag::tostring_, (Tag)(A0)(A1)
-                      , (unspecified_<A0>)(unspecified_<A1>)
+NT2_REGISTER_DISPATCH ( Tag , tag::tostring_, (Tag)(A0)(A1)(State)
+                      , (unspecified_<A0>)(unspecified_<A1>)(unspecified_<State>)
+                      )
+
+namespace nt2 { namespace ext
+{
+  template<class Tag,class Dummy>
+  struct call < Tag(tag::unspecified_, tag::unspecified_, tag::unspecified_)
+              , tag::tostring_, Dummy
+              >
+        : callable
+  {
+    typedef std::string result_type;
+
+    NT2_FUNCTOR_CALL(3)
+    {
+      return nt2::type_id<Tag>() + "("
+           + meta::compile<meta::tostring_<boost::mpl::_1> >()(a0, a2)
+           + ", "
+           + meta::compile<meta::tostring_<boost::mpl::_1> >()(a1, a2)
+           + ")";
+    }
+  };
+} }
+
+NT2_REGISTER_DISPATCH ( Tag , tag::tostring_, (Tag)(A0)(State)
+                      , (unspecified_<A0>)(unspecified_<State>)
                       )
 
 namespace nt2 { namespace ext
@@ -52,76 +80,30 @@ namespace nt2 { namespace ext
     NT2_FUNCTOR_CALL(2)
     {
       return nt2::type_id<Tag>() + "("
-           + meta::compile<meta::tostring_<boost::mpl::_1> >()(a0)
-           + ","
-           + meta::compile<meta::tostring_<boost::mpl::_1> >()(a1)
+           + meta::compile<meta::tostring_<boost::mpl::_1> >()(a0, a1)
            + ")";
     }
   };
 } }
 
-NT2_REGISTER_DISPATCH ( Tag , tag::tostring_, (Tag)(A0)
-                      , (unspecified_<A0>)
-                      )
-
-namespace nt2 { namespace ext
-{
-  template<class Tag,class Dummy>
-  struct call < Tag(tag::unspecified_)
-              , tag::tostring_, Dummy
-              >
-        : callable
-  {
-    typedef std::string result_type;
-
-    NT2_FUNCTOR_CALL(1)
-    {
-      return nt2::type_id<Tag>() + "("
-           + meta::compile<meta::tostring_<boost::mpl::_1> >()(a0)
-           + ")";
-    }
-  };
-} }
-
-NT2_REGISTER_DISPATCH ( tag::terminal_, tag::tostring_, (A0)(X)
-                       , ((simd_<arithmetic_<A0>,X>))
-                       )
-NT2_REGISTER_DISPATCH ( tag::terminal_, tag::tostring_, (A0)
-                       , (fundamental_<A0>)
+NT2_REGISTER_DISPATCH ( tag::terminal_, tag::tostring_, (A0)(A1)
+                       , (unspecified_<A0>)(unspecified_<A1>)
                        )
 
 namespace nt2 { namespace ext
 {
-  template<class Dummy,class X>
-  struct call < tag::terminal_(  tag::simd_<tag::arithmetic_,X> ) // why ?
-              , tag::tostring_, Dummy
-              >
-        : callable
-  {
-    typedef std::string result_type;
-
-    template<class Value> inline
-    result_type operator()( Value& ) const
-    {
-      return "foo";
-    }
-  };
-
-
   template<class Dummy>
-  struct call < tag::terminal_(  tag::fundamental_ )
+  struct call < tag::terminal_(  tag::unspecified_, tag::unspecified_ )
   , tag::tostring_, Dummy
   >
   : callable
   {
     typedef std::string result_type;
 
-    template<class Value> inline
-    result_type operator()( Value& v ) const
+    template<class Value, class State> inline
+    result_type operator()( Value& v, State const& s ) const
     {
-      std::ostringstream str;
-      str << v;
-      return str.str();
+      return s.get().first[s.get().second++];
     }
   };
 } }
@@ -133,10 +115,22 @@ int main()
 
     pack<float> a, b, c, d;
     
+    boost::array<const char*, 6> m = {
+		"d",
+		"c",
+		"j",
+		"c",
+		"i",
+		"a"
+	};
+	
+	std::pair< boost::array<const char*, 6>, int > state(m, 0);
+    
     std::cout << nt2::meta::compile<
       nt2::meta::tostring_<boost::mpl::_1>
     >()
     (
-      !(a + 3*~c)+4*nt2::div(c,d)
+      !(a + 3*~c)+4*nt2::div(c,d),
+      boost::ref(state)
     ) << std::endl;
 }
