149aa954abb1af19de2626a7dfbbc299e3f87cbd
Mathias Gaunard <mathias@gaunard.com>
2011-10-06 19:32:46 +0200

    avoid putting lrb prototype header in extension declaration file

--- modules/boost/simd/sdk/include/boost/simd/sdk/simd/extensions/lrb/types.hpp
+++ modules/boost/simd/sdk/include/boost/simd/sdk/simd/extensions/lrb/types.hpp
@@ -12,37 +12,37 @@
 #include <boost/simd/sdk/config/type_lists.hpp>
 
 #define BOOST_SIMD_SIMD_TYPES                                 \
-    (double)(boost::simd::int64_t)                            \
+    (double)(boost::simd::int64_t)(boost::simd::uint64_t)     \
     (float)(boost::simd::uint32_t)(boost::simd::int32_t)      \
 /**/
 
 #define BOOST_SIMD_SIMD_REAL_TYPES (double)(float)
 /**/
 
-#define BOOST_SIMD_SIMD_UNSIGNED_TYPES (boost::simd::uint32_t)  \
+#define BOOST_SIMD_SIMD_UNSIGNED_TYPES (boost::simd::uint32_t)(boost::simd::uint64_t)  \
 /**/
 
-#define BOOST_SIMD_SIMD_INTEGRAL_TYPES (boost::simd::int64_t)(boost::simd::uint32_t)  \
-                                (boost::simd::int32_t)     \
+#define BOOST_SIMD_SIMD_INTEGRAL_TYPES (boost::simd::uint64_t)(boost::simd::uint32_t)  \
+                                (boost::simd::uint32_t)(boost::simd::int32_t)     \
 /**/
 
 #define BOOST_SIMD_SIMD_SIGNED_TYPES (boost::simd::int64_t)(boost::simd::int32_t)(float)  \
 /**/
 
-#define BOOST_SIMD_SIMD_INTEGRAL_UNSIGNED_TYPES  (boost::simd::uint32_t)  \
+#define BOOST_SIMD_SIMD_INTEGRAL_UNSIGNED_TYPES  (boost::simd::uint32_t)(boost::simd::uint64_t)  \
 /**/
 
 #define BOOST_SIMD_SIMD_INTEGRAL_SIGNED_TYPES  (boost::simd::int64_t)(boost::simd::int32_t) \
 /**/
 
-#define BOOST_SIMD_SIMD_REAL_CONVERTIBLE_TYPES  (boost::simd::int64_t)(boost::simd::int32_t)(boost::simd::uint32_t)\
+#define BOOST_SIMD_SIMD_REAL_CONVERTIBLE_TYPES  (boost::simd::int64_t)(boost::simd::uint64_t)(boost::simd::int32_t)(boost::simd::uint32_t)\
                                                 (float)(double)\
 /**/
 
 #define BOOST_SIMD_SIMD_INT_CONVERT_TYPES  (boost::simd::int32_t)(boost::simd::int64_t) \
 /**/
 
-#define BOOST_SIMD_SIMD_UINT_CONVERT_TYPES  (boost::simd::uint32_t) \
+#define BOOST_SIMD_SIMD_UINT_CONVERT_TYPES  (boost::simd::uint32_t)(boost::simd::uint64_t) \
 /**/
 
 #define BOOST_SIMD_SIMD_SIGNED_INT_GT_8  (boost::simd::int32_t)(boost::simd::int64_t)  \
@@ -51,38 +51,38 @@
 #define BOOST_SIMD_SIMD__INT_CONVERT_TYPES  (boost::simd::int32_t)(boost::simd::int64_t)  \
 /**/
 
-#define BOOST_SIMD_SIMD__UINT_CONVERT_TYPES  (boost::simd::uint32_t) \
+#define BOOST_SIMD_SIMD__UINT_CONVERT_TYPES  (boost::simd::uint32_t)(boost::simd::uint64_t) \
 /**/
 
 #define BOOST_SIMD_SIMD_SIGNED_INT_GT_8 (boost::simd::int32_t)(boost::simd::int64_t)  \
 /**/
 
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8  (boost::simd::int32_t) \
+#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8  (boost::simd::int32_t)(boost::simd::uint64_t) \
 /**/
 
-#define BOOST_SIMD_SIMD_INT_64_TYPES  \
+#define BOOST_SIMD_SIMD_INT_64_TYPES  (boost::simd::int64_t)(boost::simd::uint64_t)\
 /**/
 
 #define BOOST_SIMD_SIMD_INT_32_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t) \
 /**/
 
 
-#define BOOST_SIMD_SIMD_SIGNED_INT_GT_8_TYPES  (boost::simd::int32_t) \
+#define BOOST_SIMD_SIMD_SIGNED_INT_GT_8_TYPES  (boost::simd::int32_t)(boost::simd::int64_t) \
 /**/
 
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8_TYPES (boost::simd::uint32_t) \
+#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_8_TYPES (boost::simd::uint32_t)(boost::simd::uint64_t) \
 /**/
 
 #define BOOST_SIMD_SIMD_LT64_TYPES (boost::simd::int32_t)(boost::simd::uint32_t)(float) \
 /**/
 
-#define BOOST_SIMD_SIMD_GT16_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t)(float)(double) \
+#define BOOST_SIMD_SIMD_GT16_TYPES  (boost::simd::int32_t)(boost::simd::uint32_t)(boost::simd::int64_t)(boost::simd::uint64_t)(float)(double) \
 /**/
 
 #define BOOST_SIMD_SIMD_SIGNED_INT_GT_16_TYPES  (boost::simd::int32_t)(boost::simd::int64_t)  \
 /**/
 
-#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_16_TYPES  (boost::simd::uint32_t) \
+#define BOOST_SIMD_SIMD_UNSIGNED_INT_GT_16_TYPES  (boost::simd::uint32_t)(boost::simd::uint64_t) \
 /**/
 
 #endif
