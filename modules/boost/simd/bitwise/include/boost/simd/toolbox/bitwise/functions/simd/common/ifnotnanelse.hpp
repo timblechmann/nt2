1008a7ea1efe5f77ff79f4c0479ac66b18054e9e
Jt lapreste <jtlapreste@gmail.com>
2011-10-17 09:46:29 +0200

    ifnotnanelse

--- modules/boost/simd/bitwise/include/boost/simd/toolbox/bitwise/functions/simd/common/ifnotnanelse.hpp
+++ modules/boost/simd/bitwise/include/boost/simd/toolbox/bitwise/functions/simd/common/ifnotnanelse.hpp
@@ -6,8 +6,8 @@
 //                 See accompanying file LICENSE.txt or copy at                 
 //                     http://www.boost.org/LICENSE_1_0.txt                     
 //==============================================================================
-#ifndef BOOST_SIMD_TOOLBOX_BITWISE_FUNCTIONS_SIMD_COMMON_IFNANELSE_HPP_INCLUDED
-#define BOOST_SIMD_TOOLBOX_BITWISE_FUNCTIONS_SIMD_COMMON_IFNANELSE_HPP_INCLUDED
+#ifndef BOOST_SIMD_TOOLBOX_BITWISE_FUNCTIONS_SIMD_COMMON_IFNOTNANELSE_HPP_INCLUDED
+#define BOOST_SIMD_TOOLBOX_BITWISE_FUNCTIONS_SIMD_COMMON_IFNOTNANELSE_HPP_INCLUDED
 
 #include <boost/assert.hpp>
 #include <boost/simd/sdk/meta/cardinal_of.hpp>
@@ -19,12 +19,12 @@
 /////////////////////////////////////////////////////////////////////////////
 namespace boost { namespace simd { namespace ext
 {
-  BOOST_SIMD_FUNCTOR_IMPLEMENTATION_IF ( boost::simd::tag::ifnanelse_, tag::cpu_, (A0)(A1)(X)
+  BOOST_SIMD_FUNCTOR_IMPLEMENTATION_IF ( boost::simd::tag::ifnotnanelse_, tag::cpu_, (A0)(A1)(X)
                                 , (boost::mpl::equal_to < boost::simd::meta::cardinal_of<A0>
                                                         , boost::simd::meta::cardinal_of<A1>
                                                         >
                                   )
-                                , (boost::simd::tag::ifnanelse_ ( simd_<arithmetic_<A0>,X>
+                                , (boost::simd::tag::ifnotnanelse_ ( simd_<arithmetic_<A0>,X>
                                                 , simd_<arithmetic_<A1>,X>
                                                 )
                                   )
@@ -38,7 +38,7 @@ namespace boost { namespace simd { namespace ext
     operator()(A0 const& a0, A1 const& a1) const
     {
       BOOST_ASSERT_MSG(is_simd_logical(a0), "Some entries are not legal SIMD True or False in first parameter"); 
-      return bitwise_or(a1, a0);
+      return bitwise_andnot(a1, a0);
     }
   };
 } } }
